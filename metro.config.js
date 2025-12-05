78bf9f02214211f8201302431edce48949a9f9c1
kdandy <dandybdr@gmail.com>
2025-12-05 17:33:15 +0700

    depedency updater

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,5 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(defaultConfig, {
-  resolver: {
-    assetExts: [...defaultConfig.resolver.assetExts],
-  },
-});
+module.exports = config;
